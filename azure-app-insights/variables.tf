2aa1a49bd46c9ff6b251f94cc58becf4bb4e5d9b
Rajat Raghuvanshi <rjraghu805@gmail.com>
2024-03-31 15:19:33 +0530

    updated module

--- azure-app-insights/variables.tf
+++ azure-app-insights/variables.tf
@@ -13,36 +13,14 @@ variable "location" {
   description = "The location where resources need to be build"
 }
 
-variable "enable_local_authentication" {
-  type        = bool
-  description = "Enabling local authentication, excludes AD based authentication"
-  default     = true
-}
 
 variable "tags" {
   type        = map(any)
   description = "The tag name (key=value)"
 }
 
-variable "log_analytics_resource_id" {
-  description = "The resource ID of the Log Analytics Workspace to which logs and metrics will be sent"
-  type        = string
-}
-
 variable "application_type" {
   description = "Specifies the type of Application Insights to create. Unmatched values are treated as web"
   type        = string
   default     = "other"
-}
-
-variable "daily_quota_gb" {
-  type        = number
-  description = "The instance daily quota for ingestion in GB."
-  default     = null
-}
-
-variable "daily_quota_notification" {
-  type        = bool
-  description = "Specifies if a notification email will be sent when the daily data volume cap is met."
-  default     = true
 }
